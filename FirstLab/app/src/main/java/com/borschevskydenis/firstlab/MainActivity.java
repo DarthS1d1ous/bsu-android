6de37e851a0c3272cf723c9a29ae72f2d15099b8
Denis Borschevsky <borschevsky.denis@gmail.com>
2019-10-05 17:25:28 +0300

    first Lab

---
+++ FirstLab/app/src/main/java/com/borschevskydenis/firstlab/MainActivity.java
@@ -0,0 +1,43 @@
+package com.borschevskydenis.firstlab;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("text", editText.getText().toString());
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String text = data.getStringExtra("text");
+        textView.setText(text);
+    }
+
+}
+
---
+++ FirstLab/app/src/main/java/com/borschevskydenis/firstlab/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.borschevskydenis.firstlab;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        textView.setText(intent.getStringExtra("text"));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("text", editText.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+
+            }
+        });
+
+    }
+}
